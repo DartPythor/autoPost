010caf47ec44706a923119c26bd8719183c65b22
DarthByte <neverovm28@gmail.com>
2023-07-11 21:33:08 +0500

    token secr.

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main(vk: VkApiUser, wiki: WikipediaApi, data: dict) -> None:
 
 
 if __name__ == '__main__':
-    access_token = "vk1.a.vH9qoivRrVEZc4mp10Jla0P_cZb4FNo3d-h9x4ilxHaI8EbAsylfkPot3bfdVsuoq0r2sIlcgOA0Qos4F3E9HHRctbn6WgUgEiGaAV_KkfP-b2bE3PdpE-nWVDiQrgDxQOKJ4tly2GvoZi-zxusYsHbAxPZ6hpIA9MlpiuvStCtYZspSvFBUC-q1RDooyC0kVzCwOJPiCwBhY2TAwgoOwg"
+    access_token = input()
     user_id = 456850585
     nickname = "mr.stark3"
     vk_api = VkApiUser(access_token, user_id, nickname)
