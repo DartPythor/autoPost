7cadf3b26659ceb2c7acb50f95f1b816b46cb873
Maxim <neverovm28@gmail.com>
2023-12-30 20:54:33 +0500

    fix: Add needing methods for interface

--- wikipedia/interfaceWikipediaApi.py
+++ wikipedia/interfaceWikipediaApi.py
@@ -6,5 +6,13 @@ class InterfaceWikipediaApi(metaclass=abc.ABCMeta):
     def get_pages_list(self, *args, **kwargs):
         pass
 
+    @abc.abstractmethod
+    def get_title(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def set_backup(self, *args, **kwargs):
+        pass
+
 
 __all__ = ()
