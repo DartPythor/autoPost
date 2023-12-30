2cac04a415c67b4e2214a8f92100c1295b4bd961
Maxim <neverovm28@gmail.com>
2023-12-30 21:17:49 +0500

    feat: Add default settings for .env

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import decouple
 
 FILE_STORAGE_DATA = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
-    decouple.config("FILE_STORAGE_DATA"),
+    decouple.config("FILE_STORAGE_DATA", default="data"),
 )
-FILE_NAME_PAGES = decouple.config("FILE_NAME_PAGES")
+FILE_NAME_PAGES = decouple.config("FILE_NAME_PAGES", default="pages")
