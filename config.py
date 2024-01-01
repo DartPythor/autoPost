9170cc2a07b914e7f7e36d9d5e67109fd65fc727
Maxim <neverovm28@gmail.com>
2024-01-01 21:24:49 +0500

    feat: Add access tokens in .env

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ FILE_STORAGE_DATA = os.path.join(
     decouple.config("FILE_STORAGE_DATA", default="data"),
 )
 FILE_NAME_PAGES = decouple.config("FILE_NAME_PAGES", default="pages")
+ACCESS_TOKEN_BOT = decouple.config("ACCESS_TOKEN_BOT")
+ACCESS_TOKEN_USER = decouple.config("ACCESS_TOKEN_USER")
+ADMIN_ID = decouple.config("ADMIN_ID", cast=int)
+GROUP_ID = decouple.config("GROUP_ID", cast=int)
