a08bca28aa2729de9e2f62017c10cbff8ffe557a
Maxim <neverovm28@gmail.com>
2024-01-02 17:46:30 +0500

    fix: Fix package import in bot

--- vkontakte/bot.py
+++ vkontakte/bot.py
@@ -1,15 +1,15 @@
-import vk_config
+import vkontakte.vk_config
 import vkbottle.bot
 
 import vkontakte.bot_handlers.admin
 import vkontakte.bot_handlers.middleware
 
 
-vk_config.labeler.load(vkontakte.bot_handlers.admin.admin_labeler)
-vk_config.labeler.message_view.register_middleware(
+vkontakte.vk_config.labeler.load(vkontakte.bot_handlers.admin.admin_labeler)
+vkontakte.vk_config.labeler.message_view.register_middleware(
     vkontakte.bot_handlers.middleware.ThreadInfoMiddleware,
 )
 bot = vkbottle.bot.Bot(
-    api=vk_config.bot_api,
-    labeler=vk_config.labeler,
+    api=vkontakte.vk_config.bot_api,
+    labeler=vkontakte.vk_config.labeler,
 )
