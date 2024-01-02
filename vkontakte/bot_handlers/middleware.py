9eb8987059b7a2e71f10f6cec98393aa208c6587
Maxim <neverovm28@gmail.com>
2024-01-02 17:45:36 +0500

    feat: Add middleware thread getter and setter

--- vkontakte/bot_handlers/middleware.py
+++ vkontakte/bot_handlers/middleware.py
@@ -4,8 +4,17 @@ import vkbottle.bot
 class ThreadInfoMiddleware(
     vkbottle.BaseMiddleware[vkbottle.bot.Message],
 ):
+    thread = None
+
     async def pre(self):
-        self.send({"thread": True})
+        self.send({"thread": self.get_thread()})
+
+    @staticmethod
+    def set_thread(thread):
+        ThreadInfoMiddleware.thread = thread
+
+    def get_thread(self):
+        return self.thread
 
 
 __all__ = ()
