23b23d938d104926035467bf9877dcd4c02b84fe
Maxim <neverovm28@gmail.com>
2024-01-02 23:09:05 +0500

    feat: Class controller for chatGPT

---
+++ chatGPT/chat_gpt.py
@@ -0,0 +1,30 @@
+import os.path
+
+import g4f
+
+import config
+
+
+class ChatGPT(g4f.ChatCompletion):
+    def __init__(self, model=g4f.models.gpt_4, role="user", filename="chatGPT_message.txt"):
+        self.model = model
+        self.role = role
+        self.filename = filename
+
+    def get_template(self, theme):
+        with open(
+            os.path.join(
+                config.FILE_STORAGE_TEMPLATES,
+                self.filename,
+            ),
+            mode="r",
+            encoding="utf-8",
+        ) as file:
+            template = file.readline()
+        return template.format(theme=theme)
+
+    def make_post_content(self, theme):
+        return self.create(
+            model=self.model,
+            messages=[{"role": self.role, "content": self.get_template(theme)}],
+        )
