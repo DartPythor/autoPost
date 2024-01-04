94a65330eca70434dd68e374ac250c32d3a1eaa7
Maxim <neverovm28@gmail.com>
2024-01-04 23:33:38 +0500

    feat: Add main.py for start app

---
+++ main.py
@@ -0,0 +1,12 @@
+import config
+import poster.poster_thread
+import vkontakte.bot_handlers.middleware
+import vkontakte.bot
+
+
+thread_post = poster.poster_thread.PosterThread(
+    config.DAY_POST, config.NIGHT_POST, config.TIMEZONE
+)
+thread_post.start()
+vkontakte.bot_handlers.middleware.ThreadInfoMiddleware.set_thread(thread_post)
+vkontakte.bot.bot.run_forever()
