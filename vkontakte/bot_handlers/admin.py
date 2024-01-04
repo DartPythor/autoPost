578c884d9008261edd2e2f420267f02e8e83facb
Maxim <neverovm28@gmail.com>
2024-01-04 23:33:04 +0500

    feat: Add methods for bots in api

--- vkontakte/bot_handlers/admin.py
+++ vkontakte/bot_handlers/admin.py
@@ -11,36 +11,65 @@ admin_labeler.auto_rules = [
 
 @admin_labeler.message(vkbottle.bot.rules.PayloadRule({"command": "start"}))
 async def start(message: vkbottle.bot.Message, thread):
+    server_log = f"Состояние сервера: {thread.is_alive()}"
+    server_log += "✅" if thread.is_alive() else "❌"
+
+    poster_log = f"Состояние постера: {thread.is_stop()}"
+    poster_log += "✅" if thread.is_stop() else "❌"
+
+    all_log = "\n".join(
+        (
+            server_log,
+            poster_log,
+        ),
+    )
     await message.answer(
-        message="Сервер запущен.",
+        message=all_log,
         keyboard=vkontakte.bot_handlers.keyboard.keyboard.get_json(),
     )
 
 
 @admin_labeler.message(vkbottle.bot.rules.PayloadRule({"command": "stop"}))
 async def stop(message: vkbottle.bot.Message, thread):
-    await message.answer("stop")
+    thread.stop()
+    await message.answer(
+        message="Сервер отключен ❌",
+        keyboard=vkbottle.EMPTY_KEYBOARD,
+    )
+    exit(0)
 
 
 @admin_labeler.message(vkbottle.bot.rules.PayloadRule({"command": "start_post"}))
 async def start_post(message: vkbottle.bot.Message, thread):
-    await message.answer("start_post")
+    thread.start_post()
+    await message.answer("Постинг включен ✅")
 
 
 @admin_labeler.message(vkbottle.bot.rules.PayloadRule({"command": "stop_post"}))
 async def stop_post(message: vkbottle.bot.Message, thread):
-    await message.answer("stop_post")
+    thread.stop_post()
+    await message.answer("Постинг выключен ❌")
 
 
 @admin_labeler.message(vkbottle.bot.rules.PayloadRule({"command": "log"}))
 async def log(message: vkbottle.bot.Message, thread):
-    await message.answer("log")
+    thread_log = "\n"
+    thread_log = thread_log.join(
+        (
+            f"Состояние сервера: {thread.is_alive()}",
+            f"Состояние постера: {thread.is_stop()}",
+            f"Временная зона: {thread.timezone}",
+            f"Время постинга днем: {thread.day_post}",
+            f"Время постинга вечером: {thread.night_post}",
+        ),
+    )
+    await message.answer(message=thread_log)
 
 
 @admin_labeler.message()
 async def keyboard(message: vkbottle.bot.Message, thread):
     await message.answer(
-        message="Нате",
+        message=f"Обновление клавиатуры\nСостояние сервера: {thread.is_alive()}",
         keyboard=vkontakte.bot_handlers.keyboard.keyboard.get_json(),
     )
 
